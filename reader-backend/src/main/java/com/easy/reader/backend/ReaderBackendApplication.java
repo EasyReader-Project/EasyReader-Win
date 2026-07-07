18660a420c2ff912c6851a8f0d75cf353508fcc5
star5470@163.com <star5470>
2026-07-07 10:40:03 +0800

    创建EasyReader-Win的Java服务端项目，删除错误的模块

---
+++ reader-backend/src/main/java/com/easy/reader/backend/ReaderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.easy.reader.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReaderBackendApplication.class, args);
+    }
+
+}
---
+++ reader-backend/src/test/java/com/easy/reader/backend/ReaderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.reader.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
